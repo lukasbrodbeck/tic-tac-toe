7464fd8afbaef4656fcee02e8d668987bcd84992
Lukas Brodbeck <lukasbrodbeck@gmail.com>
2019-09-06 14:04:03 +0200

    Build a simple check for the winner

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 {
-  let field = 0;
   let currentTurn = 'X';
   let selectedPlayerX = [];
   let selectedPlayerO = [];
+  let winningPossibilityArray = [];
+  winningPossibilityArray[0] = [1, 2, 3];
+  winningPossibilityArray[1] = [4, 5, 6];
+  winningPossibilityArray[2] = [7, 8, 9];
+  winningPossibilityArray[3] = [1, 4, 7];
+  winningPossibilityArray[4] = [2, 5, 8];
+  winningPossibilityArray[5] = [3, 6, 9];
+  winningPossibilityArray[6] = [1, 5, 9];
+  winningPossibilityArray[7] = [3, 5, 7];
 
   buildPlayField();
 
@@ -22,25 +30,52 @@
       let currentPosition = currentItem.data('position');
       currentItem.text(currentTurn);
       e.currentTarget.setAttribute('data-tagged', currentTurn);
+
       if (currentTurn === 'X') {
         selectedPlayerX.push(currentPosition);
       } else {
         selectedPlayerO.push(currentPosition)
       }
-      checkForWinner();
-      changeTurn();
 
+      if (checkForWinner(currentPosition)) {
+        endGame();
+      }
+
+      changeTurn();
     } else {
+      //TODO display invalid turn
     }
-    console.log(selectedWidgets);
   }
 
-  function checkForWinner() {
+  function checkForWinner(currentPosition) {
+    let currentArray = [];
     if (currentTurn === 'X') {
-      selectedPlayerX.push(currentPosition);
+      currentArray = selectedPlayerX;
     } else {
-      selectedPlayerO.push(currentPosition)
+      currentArray = selectedPlayerO;
     }
+    for (let e of winningPossibilityArray) {
+      if (e.includes(currentPosition)) {
+        let counter = 0;
+
+        for (let element of e) {
+          if (currentArray.includes(element)) {
+            counter++;
+          }
+        }
+
+        if (counter === 3) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  function endGame() {
+    console.log(currentTurn + ' has won the game.');
+    //TODO build a sequence to end the game
   }
 
   function changeTurn() {
