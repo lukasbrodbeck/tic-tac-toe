4a95d68e417689210418d3cdbc972161c9b8f406
Lukas Brodbeck <lukasbrodbeck@gmail.com>
2019-09-08 15:15:49 +0200

    Refactor player config and winning condition
    Necessary for npc implementation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 {
-  let currentTurn = 'X';
-  let selectedPlayerX = [];
-  let selectedPlayerO = [];
+  let currentTurn = 1;
+  let selectedArray = [];
   let $infoBox = $('#info');
   let $errorBox = $('#error');
   let $startButton = $('#start-button');
@@ -22,17 +21,16 @@
 
   function startGame() {
     $playField.html('');
-    selectedPlayerX = [];
-    selectedPlayerO = [];
     buildPlayField();
     isStarted = true;
     turnCounter = 0;
-    currentTurn = 'X';
+    currentTurn = 1;
   }
 
   function buildPlayField() {
     for (let position = 1; position < 10; position++) {
       $playField.append('<div class="widget" data-position="' + position + '" data-tagged="false"></div>');
+      selectedArray[position] = 0;
     }
     $playField.find('.widget').on('click', (e) => selectWidget(e));
   }
@@ -42,16 +40,11 @@
 
     if (isStarted && $currentItem.data('tagged') === false) {
       let currentPosition = $currentItem.data('position');
-      $currentItem.addClass(currentTurn.toLowerCase());
+      $currentItem.addClass(getCurrentPlayer().toLowerCase());
       $currentItem.data('tagged', true);
       turnCounter++;
       $errorBox.text('');
-
-      if (currentTurn === 'X') {
-        selectedPlayerX.push(currentPosition);
-      } else {
-        selectedPlayerO.push(currentPosition)
-      }
+      selectedArray[currentPosition] = currentTurn;
 
       if (checkForWinner(currentPosition)) {
         endGame(true);
@@ -70,27 +63,17 @@
   }
 
   function checkForWinner(currentPosition) {
-    let currentArray = [];
-    if (currentTurn === 'X') {
-      currentArray = selectedPlayerX;
-    } else {
-      currentArray = selectedPlayerO;
-    }
     for (let e of winningPossibilityArray) {
-      if (e.includes(currentPosition)) {
-        let counter = 0;
-
+      let counter = 0;
         for (let element of e) {
-          if (currentArray.includes(element)) {
-            counter++;
+          counter += selectedArray[element];
+
+          if (counter === 3 || counter === -3) {
+            markWinner(e);
+
+            return true;
           }
         }
-
-        if (counter === 3) {
-          markWinner(e);
-          return true;
-        }
-      }
     }
 
     return false;
@@ -99,15 +82,19 @@
   function endGame(isWinner) {
     isStarted = false;
     if (isWinner) {
-      $infoBox.text('Der Spieler ' + currentTurn + ' hat das Spiel gewonnen');
+      $infoBox.text('Der Spieler ' + getCurrentPlayer() + ' hat das Spiel gewonnen');
     } else {
       $infoBox.text('Unentschieden. Es ist kein Zug mehr möglich.');
     }
   }
 
   function changeTurn() {
-    currentTurn = currentTurn ==='X' ? 'O' : 'X';
-    $infoBox.text('Der Spieler ' + currentTurn + ' ist an der Reihe');
+    currentTurn = -currentTurn;
+    $infoBox.text('Der Spieler ' + getCurrentPlayer() + ' ist an der Reihe');
+  }
+
+  function getCurrentPlayer() {
+    return currentTurn === 1 ? 'X' : 'O';
   }
 
   function markWinner(e) {
