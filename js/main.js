b2ec630910d548539de476e92e96b33569400573
Lukas Brodbeck <lukasbrodbeck@gmail.com>
2019-10-05 03:27:09 +0200

    Clean main according to the linting rules

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-{
+import jQuery from 'jQuery';
+
+jQuery(document).ready(function ($) {
   let currentTurn;
-  let selectedArray = [];
-  let $infoBox = $('#info');
-  let $errorMessage = $('.error-message');
-  let $startButton = $('#start-button');
-  let $playField = $('#play-field');
-  let $startingCheckbox = $('#starting-checkbox');
+  const selectedArray = [];
+  const $infoBox = $('#info');
+  const $errorMessage = $('.error-message');
+  const $startButton = $('#start-button');
+  const $playField = $('#play-field');
+  const $startingCheckbox = $('#starting-checkbox');
   let turnCounter = 0;
   let isStarted = false;
   // there are 3 difficulty levels, 1: total random movement, 2: tries to win/prevent if possible, 3: makes you cry
   let difficultyLevel;
-  let winningPossibilityArray = [];
+  const winningPossibilityArray = [];
   winningPossibilityArray[0] = [1, 2, 3];
   winningPossibilityArray[1] = [4, 5, 6];
   winningPossibilityArray[2] = [7, 8, 9];
@@ -20,7 +22,7 @@
   winningPossibilityArray[6] = [1, 5, 9];
   winningPossibilityArray[7] = [3, 5, 7];
 
-  //semantic UI - instantiating of form + popup message
+  // semantic UI - instantiating of form + popup message
   $('.ui.select-field').dropdown();
   $('.ui.checkbox').checkbox();
   $errorMessage.find('.close')
@@ -31,7 +33,7 @@
   /**
    * Start a new game
    */
-  function startGame() {
+  function startGame () {
     $playField.html('');
     currentTurn = $startingCheckbox.hasClass('checked') ? -1 : 1;
     isStarted = true;
@@ -49,7 +51,7 @@
   /**
    * Build the playfield
    */
-  function buildPlayField() {
+  function buildPlayField () {
     for (let position = 1; position < 10; position++) {
       $playField.append('<div class="field-box" data-position="' + position + '"></div>');
       selectedArray[position] = 0;
@@ -61,8 +63,8 @@
    * Player clicks on a field
    * @param e
    */
-  function selectField(e) {
-    let currentPosition = $(e.currentTarget).data('position');
+  function selectField (e) {
+    const currentPosition = $(e.currentTarget).data('position');
 
     if (isStarted && selectedArray[currentPosition] === 0) {
       makeAMove(currentPosition);
@@ -79,7 +81,7 @@
    * Apply styling to the chosen field
    * @param position
    */
-  function markField(position) {
+  function markField (position) {
     $playField.find(`[data-position='${position}']`).addClass(getCurrentPlayer().toLowerCase());
     toggleErrorMessage();
   }
@@ -106,9 +108,9 @@
    * Check whether someone won and the game is over
    * @returns {boolean}
    */
-  function checkForWinner() {
-    for (let winningPossibility of winningPossibilityArray) {
-      let counter = arraySum(winningPossibility);
+  function checkForWinner () {
+    for (const winningPossibility of winningPossibilityArray) {
+      const counter = arraySum(winningPossibility);
       if (counter === 3 || counter === -3) {
         markWinner(winningPossibility);
 
@@ -123,7 +125,7 @@
    * Manage the end of the game
    * @param isWinner
    */
-  function endGame(isWinner) {
+  function endGame (isWinner) {
     isStarted = false;
     if (isWinner) {
       $infoBox.text('Der Spieler ' + getCurrentPlayer() + ' hat das Spiel gewonnen');
@@ -135,7 +137,7 @@
   /**
    * Change the player
    */
-  function changeTurn() {
+  function changeTurn () {
     currentTurn = -currentTurn;
     $infoBox.text('Der Spieler ' + getCurrentPlayer() + ' ist an der Reihe');
     if (difficultyLevel > 0 && currentTurn === -1) {
@@ -146,7 +148,7 @@
   /**
    * @returns {string}
    */
-  function getCurrentPlayer() {
+  function getCurrentPlayer () {
     return currentTurn === 1 ? 'X' : 'O';
   }
 
@@ -154,8 +156,8 @@
    * Highlights the winning fields
    * @param winningArray
    */
-  function markWinner(winningArray) {
-    for (let position of winningArray) {
+  function markWinner (winningArray) {
+    for (const position of winningArray) {
       $playField.find(`[data-position='${position}']`).addClass('winning');
     }
   }
@@ -164,26 +166,29 @@
    * Let the AI do his thing
    * Depending on the difficulty level there are multiple calculations
    */
-  function npcMove() {
+  function npcMove () {
     let necessaryAction = [];
     if (difficultyLevel >= 2) {
       // we run for each condition, so we can prioritize winning over prevention
-      condition:
-      for (let condition of [-2, 2]) {
-        for (let winningPossibility of winningPossibilityArray) {
-          let counter = arraySum(winningPossibility);
+      for (const condition of [-2, 2]) {
+        for (const winningPossibility of winningPossibilityArray) {
+          const counter = arraySum(winningPossibility);
 
           if (counter === condition) {
             necessaryAction = winningPossibility;
 
-            break condition;
+            break;
           }
+        }
+
+        if (necessaryAction.length > 0) {
+          break;
         }
       }
     }
 
     if (necessaryAction.length > 0) {
-      for (let field of necessaryAction) {
+      for (const field of necessaryAction) {
         if (selectedArray[field] === 0) {
           makeAMove(field);
           break;
@@ -192,7 +197,7 @@
     } else {
       let searchRandomField = true;
       while (searchRandomField === true) {
-        let randomField = randomIntFromInterval(1, 9);
+        const randomField = randomIntFromInterval(1, 9);
         if (selectedArray[randomField] === 0) {
           makeAMove(randomField);
           searchRandomField = false;
@@ -207,7 +212,7 @@
    * @param {number} max
    * @returns {number}
    */
-  function randomIntFromInterval(min = 1, max = 1) {
+  function randomIntFromInterval (min = 1, max = 1) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
@@ -215,7 +220,7 @@
    * @param array
    * @returns {number}
    */
-  function arraySum(array = [0, 0, 0]) {
+  function arraySum (array = [0, 0, 0]) {
     return selectedArray[array[0]] + selectedArray[array[1]] + selectedArray[array[2]];
   }
 
@@ -223,11 +228,11 @@
    * If empty string is provided as errorMessage, hide the message
    * @param {string} errorMessage
    */
-  function toggleErrorMessage(errorMessage = '') {
+  function toggleErrorMessage (errorMessage = '') {
     // .transition() already toggles the message, so we need to check whether we really want to trigger it or not
     if ((errorMessage !== '' && !$errorMessage.hasClass('visible')) || (errorMessage === '' && $errorMessage.hasClass('visible') && !$errorMessage.hasClass('zoom'))) {
       $errorMessage.transition('zoom', 200);
       $errorMessage.find('#error-text').text(errorMessage);
     }
   }
-}
+});
